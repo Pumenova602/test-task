23d3450e0f51e519e7b5851eaffe009c6b3660d4
pumenova602 <pumenova602@gmail.com>
2020-07-31 10:35:03 +0300

    updated version

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -11,10 +11,19 @@ export default class TodoListItem extends Component {
     this.setState(({done}) => {
       return {
         done: !done
-        
       };
     });
-  };
+  }
+  todoLabel = ({label}) => {
+      return (
+        <span
+            className="todo-list-item-label"
+            onClick={ this.onLabelClick }>
+            {label}
+      </span>)
+    
+  }
+
 
   render() {
     const { label, onDelete } = this.props;
@@ -22,20 +31,31 @@ export default class TodoListItem extends Component {
 
     let classNames = 'todo-list-item';
     if (done) {
-      classNames += ' done';
+      classNames += ' done' ;
     }
     return (
       <span className={classNames}>
-        <span
-          className="todo-list-item-label"
-          onClick={ this.onLabelClick }>
-          {label}
-        </span>
-        <button type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-          onClick={onDelete}>
-          Delete
-        </button>
+      
+        {!done 
+        ? <span
+            className="todo-list-item-label"
+            onClick={ this.onLabelClick }>
+            {label}
+      </span>
+      : <div>
+          {<span
+            className="todo-list-item-label"
+            onClick={ this.onLabelClick }>
+            {label}
+          </span>}
+          <button type="button"
+            className="btn btn-outline-danger btn-sm float-right"
+            onClick={onDelete}>
+            Delete
+          </button>
+        </div>
+        }
+        
       </span>
     );
   };
